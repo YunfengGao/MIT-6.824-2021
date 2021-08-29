247342507cd360c142cf91f489a7bc5d22bc7753
gaoyunfeng <gaoyunfeng1995@gmail.com>
2021-08-29 16:01:51 +0800

    添加非线程安全的VoteCount1.java

---
+++ lec2/vote/VoteCount1.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+
+public class VoteCount1 {
+    private static class UnsafeCounter {
+        private int count;
+        private int finish;
+
+        private void addCount() {
+            count++;
+        }
+
+        private int getCount() {
+            return count;
+        }
+
+        private void addFinish() {
+            finish++;
+        }
+
+        private int getFinish() {
+            return finish;
+        }
+    }
+
+    public static void main(String[] args) {
+        UnsafeCounter unsafeCounter = new UnsafeCounter();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                boolean vote = requestVote();
+                if (vote) {
+                    unsafeCounter.addCount();
+                }
+                unsafeCounter.addFinish();
+            }).start();
+        }
+
+        for (; ; ) {
+            if (unsafeCounter.getCount() >= 5 || unsafeCounter.getFinish() == 10) {
+                break;
+            }
+        }
+
+        System.out.println(unsafeCounter.getCount() >= 5 ? "received 5+ votes!" : "lost");
+    }
+
+    private static boolean requestVote() {
+        return new Random().nextBoolean();
+    }
+}
