296a9eece08dd6743461550b625d4d47d012772c
gaoyunfeng <gaoyunfeng1995@gmail.com>
2021-09-18 01:13:09 +0800

    新增测试脚本

---
+++ lab1/src/main/test-mr.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#
+# basic map-reduce test
+#
+
+# run the test in a fresh sub-directory.
+rm -rf mr-tmp
+mkdir mr-tmp || exit 1
+cd mr-tmp || exit 1
+rm -f mr-*
+
+# make sure software is freshly built.
+(javac ../MrSequential.java ../../mr/KeyValue.java ../../mrapp/WordCount.java -d out) || exit 1
+
+failed_any=0
+
+#########################################################
+# first word-count
+
+# generate the correct output
+java -classpath out main.MrSequential ../../../data || exit 1
+sort mr-out-0 > mr-correct-wc.txt
+rm -f mr-out*
+
+echo '***' Starting wc test.
+
+
+#########################################################
+if [ $failed_any -eq 0 ]; then
+    echo '***' PASSED ALL TESTS
+else
+    echo '***' FAILED SOME TESTS
+    exit 1
+fi
